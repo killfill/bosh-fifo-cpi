5b93062fffbb35ca30d86e62d07c08b774decae7
Phillip Neumann <pneumann@gmail.com>
2014-08-11 10:02:44 -0400

    Save metadata in VM customer_metadata so the value are accesible from inside the VM

--- methods/set_vm_metadata.js
+++ methods/set_vm_metadata.js
@@ -1,10 +1,39 @@
-//TODO
 module.exports = function(fifo, args, response) {
-	var id = args[0]
+	var vm = args[0],
+		hash = args[1]
 
-	response({
-		error: null,
-		log: 'set_vm_metadata_result ' + id + '. Interesting.. we could use the fifo metadata... What is this for?',
-		result: 'set_vm_metadata_result'
+	var n = Object.keys(hash).length
+
+
+	function cb(err, res) {
+
+		if (err || res.statusCode >= 300)
+			return response({
+				error: {
+					type: 'Body::Clouds::CloudError',
+					message: 'Could not set metadata ' + (err && err.message || res.body || res.statusCode),
+					ok_to_retry: false
+				},
+				log: 'Could not set metadata' + (err && err.message || res.body || res.statusCode),
+				result: null
+			})
+
+		if (--n<=0)
+			return response({
+				result: 'Metadata setted up',
+				error: null,
+				log: 'Metadata setted up'
+			})
+	}
+
+
+	Object.keys(hash).forEach(function(k) {
+		// var obj = {args: [vm, 'metadata', 'bosh']} //Fifo metadata
+		var obj = {args: vm} //Use smartos metadata, so the values can be read from inside the VM.
+		obj.body = {}
+		obj.body[k] = hash[k]
+
+		fifo.send('vms').put(obj, cb)
 	})
+
 }
